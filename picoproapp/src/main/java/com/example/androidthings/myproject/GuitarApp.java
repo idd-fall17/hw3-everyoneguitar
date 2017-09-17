37a9101faf75f9188ef02a7b1a24a950e6d51684
Steve Trush <strush@berkeley.edu>
2017-09-16 17:16:55 -0700

    Working chords, accelerometer, and pitch change.

--- picoproapp/src/main/java/com/example/androidthings/myproject/GuitarApp.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/GuitarApp.java
@@ -26,12 +26,14 @@ public class GuitarApp extends SimplePicoPro {
     private Gpio buttonDown = GPIO_174;
     private Gpio buttonLeft = GPIO_173;
     private Gpio buttonRight = GPIO_10;
-    private Gpio buttonEnter = GPIO_32;
-    private Gpio buttonDelete = GPIO_175;
 
     //keep track of time for debouncing
-    private long lastTime = 0;
-    private final long debounceTime = 100;
+    private long lastStrumTime = 0;
+    private final long strumDebounceTime = 200;
+
+    //keep track of time for debouncing
+    private long lastChordTime = 0;
+    private final long chordDebounceTime = 100;
 
 
     SerialMidi serialMidi;
@@ -47,18 +49,27 @@ public class GuitarApp extends SimplePicoPro {
     int pitch_min = 8192;
     int pitch_max = 16000;
 
-    float play_threshhold = .6f;
-    float play_twidth = .2f;
-    boolean played = false;
-    boolean resonating = false;
+    float play_threshhold = 1.5f;
+    float play_twidth = 1.0f;
 
+    boolean isStrummed = false;
+    boolean isResonating = false;
     boolean unpitched = true;
+    boolean isOn = false;
+
     boolean pressedC = false;
-    boolean pressedE = false;
+    boolean pressedF = false;
     boolean pressedG = false;
+    boolean pressedAm = false;
+
+    long CTime = 0;
+    long FTime = 0;
+    long GTime = 0;
+    long ATime = 0;
 
     Mma8451Q accelerometer;
 
+    boolean testing = false;
     @Override
     public void setup() {
         //Sets the PinModes and the EdgeTriggers
@@ -68,38 +79,56 @@ public class GuitarApp extends SimplePicoPro {
         pinMode(buttonDown, Gpio.DIRECTION_IN);
         pinMode(buttonLeft, Gpio.DIRECTION_IN);
         pinMode(buttonRight, Gpio.DIRECTION_IN);
-        pinMode(buttonEnter, Gpio.DIRECTION_IN);
-        pinMode(buttonDelete, Gpio.DIRECTION_IN);
 
         uartInit(UART6, 115200);
         serialMidi = new SerialMidi(UART6);
 
-        try {
-            accelerometer = new Mma8451Q("I2C1");
-            accelerometer.setMode(Mma8451Q.MODE_ACTIVE);
-        } catch (IOException e) {
-            Log.e("GuitarApp", "setup", e);
+        if(true) {
+            try {
+                accelerometer = new Mma8451Q("I2C1");
+                accelerometer.setMode(Mma8451Q.MODE_ACTIVE);
+            } catch (IOException e) {
+                Log.e("GuitarApp", "setup", e);
+            }
         }
     }
 
     @Override
     public void loop() {
-        float a0 = analogRead(A0);
-        pitch_adjust(a0);
-        tone_adjust();
-        strum();
 
-//        timbre_value+=5;
-//        if(timbre_value>=127)
-//            timbre_value=0;
-//        serialMidi.midi_controller_change(channel,timbre_controller,timbre_value);
-
+        if(testing) {
+            if (!pressedC) {
+                pressedC = true;
+                pressedAm = false;
+            } else {
+                pressedC = false;
+                pressedAm = true;
+            }
+            //delay(1000);
+        }else{
+            tone_adjust();
+        }
+        if(isChord()) {
+            float a0 = analogRead(A0);
+            pitch_adjust(a0);
+            //print("Chord is held!");
+            strum(false);
+        }else {
+            turn_off_notes();
+        }
     }
 
-    private void strum() {
+    private void strum(boolean testing) {
         float[] xyz = {0.f, 0.f, 0.f};
         try {
-            xyz = accelerometer.readSample();
+            if(!testing)
+                xyz = accelerometer.readSample();
+            else {
+                if(millis() % 3 == 0) {
+                    xyz[0] = 0.7f;
+                    xyz[1] = 0.7f;
+                }
+            }
             //println("X: "+xyz[0]+"   Y: "+xyz[1]+"   Z: "+xyz[2]);
 
             //use this line instead for unlabeled numbers separated by tabs that work with Arduino's SerialPlotter:
@@ -107,26 +136,27 @@ public class GuitarApp extends SimplePicoPro {
             float y_dir = xyz[1];
             float x_dir = xyz[0];
             //println("Y: " + y_dir);
-            if ((y_dir > play_threshhold || x_dir > play_threshhold) && played == false) {
+            if ((Math.abs(y_dir) > play_threshhold || Math.abs(x_dir) > play_threshhold) && isStrummed == false) {
                 println("Played! Y: " + y_dir);
                 println("Played! X: " + x_dir);
                 //float diff = (float) (y_dir - play_threshhold) / (float) (play_upper - play_threshhold);
                 //float result = velocity_min + ((velocity_max - velocity_min) * diff);
-                played = true;
-                resonating = true;
-                lastTime = millis();
+                isStrummed = true;
+                isResonating = true;
+                lastStrumTime= millis();
                 play_notes();
             } else {
-                if ((y_dir < (play_threshhold - play_twidth) && x_dir < (play_threshhold - play_twidth)) && millis() > (lastTime + debounceTime)) {
-                    played = false;
+                if ((y_dir < (play_threshhold - play_twidth) && x_dir < (play_threshhold - play_twidth)) && millis() > (lastStrumTime + strumDebounceTime)) {
+                    //print("Strum stopped!");
+                    isStrummed = false;
                 }
             }
-            if(resonating == true && millis() > lastTime + 2000){
+            if(isResonating == true && millis() > lastStrumTime + 2000){
                 turn_off_notes();
-                resonating = false;
+                isResonating = false;
             }
         } catch (IOException e) {
-            //Log.e("HW3Template", "loop", e);
+            Log.e("GuitarApp", "strum", e);
         }
     }
 
@@ -150,67 +180,94 @@ public class GuitarApp extends SimplePicoPro {
     }
 
     private void play_notes() {
+        turn_off_notes();
+        if(isChord()) isOn = true;
         if (pressedC) {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_C4, 127);
             serialMidi.midi_note_on(channel, SerialMidi.MIDI_C4, velocity);
-            this.printStringToScreen("C");
-        } else {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_C4, 127);
-        }
-        if (pressedE) {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_E4, 127);
             serialMidi.midi_note_on(channel, SerialMidi.MIDI_E4, velocity);
-            this.printStringToScreen("E");
-        } else {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_E4, 127);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_G4, velocity);
+            this.printStringToScreen("C ");
+        }
+        if (pressedF) {
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_F4, velocity);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_A4, velocity);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_C4, velocity);
+            this.printStringToScreen("F ");
         }
         if (pressedG) {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_G4, 127);
             serialMidi.midi_note_on(channel, SerialMidi.MIDI_G4, velocity);
-            this.printStringToScreen("G");
-        } else {
-            serialMidi.midi_note_off(channel, SerialMidi.MIDI_G4, 127);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_B4, velocity);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_D4, velocity);
+            this.printStringToScreen("G ");
         }
-//        if (pressedB) {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_B4, 127);
-//            serialMidi.midi_note_on(channel, SerialMidi.MIDI_B4, velocity);
-//            this.printStringToScreen("G");
-//        } else {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_B4, 127);
-//        }
-//        if (pressedD) {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_D4, 127);
-//            serialMidi.midi_note_on(channel, SerialMidi.MIDI_D4, velocity);
-//            this.printStringToScreen("G");
-//        } else {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_D4, 127);
-//        }
-//        if (pressedA) {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_A4, 127);
-//            serialMidi.midi_note_on(channel, SerialMidi.MIDI_A4, velocity);
-//            this.printStringToScreen("G");
-//        } else {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_A4, 127);
-//        }
-//        if (pressedF) {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_F4, 127);
-//            serialMidi.midi_note_on(channel, SerialMidi.MIDI_F4, velocity);
-//            this.printStringToScreen("G");
-//        } else {
-//            serialMidi.midi_note_off(channel, SerialMidi.MIDI_F4, 127);
-//        }
+        if (pressedAm) {
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_A4, velocity);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_C4, velocity);
+            serialMidi.midi_note_on(channel, SerialMidi.MIDI_E4, velocity);
+            this.printStringToScreen("Am ");
+        }
     }
 
     private void turn_off_notes(){
-        serialMidi.midi_note_off(channel, SerialMidi.MIDI_C4, 127);
-        serialMidi.midi_note_off(channel, SerialMidi.MIDI_E4, 127);
-        serialMidi.midi_note_off(channel, SerialMidi.MIDI_G4, 127);
+        if(isOn) {
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_C4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_E4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_G4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_B4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_D4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_A4, 127);
+            serialMidi.midi_note_off(channel, SerialMidi.MIDI_F4, 127);
+            isOn = false;
+        }
+    }
+
+    private boolean isChord(){
+        return pressedAm || pressedC || pressedF || pressedG;
     }
 
     private void tone_adjust(){
-        pressedC = !digitalRead(buttonLeft);
-        pressedE = !digitalRead(buttonRight);
-        pressedG = !digitalRead(buttonUp);
+        long currentTime = millis();
+        if(currentTime >= lastChordTime + chordDebounceTime) {
+            lastChordTime = currentTime;
+            if (!digitalRead(buttonLeft)) {
+                if (CTime <= 0) {
+                    CTime = currentTime;
+                }
+            } else {
+                CTime = 0;
+            }
+            if (!digitalRead(buttonRight)) {
+                if (FTime <= 0) {
+                    FTime = currentTime;
+                }
+            } else {
+                FTime = 0;
+            }
+            if (!digitalRead(buttonUp)) {
+                if (GTime <= 0) {
+                    GTime = currentTime;
+                }
+            } else {
+                GTime = 0;
+            }
+            if (!digitalRead(buttonDown)) {
+                if (ATime <= 0) {
+                    ATime = currentTime;
+                }
+            } else {
+                ATime = 0;
+            }
+            pressedC = pressedG = pressedF = pressedAm = false;
+            if (CTime != 0 && CTime >= FTime && CTime >= GTime && CTime >= ATime) {
+                pressedC = true;
+            } else if (GTime != 0 && GTime > CTime && GTime > FTime && GTime > ATime) {
+                pressedG = true;
+            } else if (FTime != 0 && FTime > CTime && FTime > GTime && FTime > ATime) {
+                pressedF = true;
+            } else if (ATime != 0 && ATime > CTime && ATime > FTime && ATime > GTime) {
+                pressedAm = true;
+            }
+        }
     }
 
     @Override
